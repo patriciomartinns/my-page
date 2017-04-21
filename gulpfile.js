45b599487b3759c72c89eb4fb00eaa550a92aabb
patriciomartinns <eupatriciomartins@gmail.com>
2017-04-21 17:49:48 -0300

    Minificando imagens e modificando o gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,9 @@ var gulp = require('gulp'),
     imagemin = require('gulp-imagemin');
 
 gulp.task('compress-images', function() {
-    return gulp.src('img/*')
+    return gulp.src('img/**/*')
         .pipe(imagemin({ progressive: true }))
         .pipe(gulp.dest('img'));
 });
 
-gulp.task('watch', function(){
-	gulp.watch('img/*', ['compress-images']);
-});
-
-gulp.task('default', ['compress-images', 'watch']);
+gulp.task('default', ['compress-images']);
